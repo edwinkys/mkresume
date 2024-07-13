98a6845398989f56003e8b8569a479d93744fb7c
Edwin Kys <itsedwinkys@gmail.com>
2024-07-13 15:46:20 -0500

    feat: initial impl convert to pdf

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import typer
 from src.content import Content
+from src.template import Template
 from typing_extensions import Annotated
 
+app = typer.Typer()
 
+
+@app.command()
 def main(
-    path: Annotated[str, typer.Option(help="Path to YML file")],
+    input: Annotated[str, typer.Option(help="Path to YML file")],
+    output: Annotated[str, typer.Option(help="Directory to output PDF")],
     template: Annotated[str, typer.Option(help="Template name")] = "default"
 ):
-    Content(path=path)
+    content = Content(path=input)
+    template = Template(content=content, name=template)
+    template.to_pdf(output)
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
--- src/template.py
+++ src/template.py
@@ -1,2 +1,36 @@
+import os
+from jinja2 import Environment, FileSystemLoader
+from weasyprint import HTML
+from src.content import Content
+from typing import Optional
+
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATES_DIR = os.path.join(ROOT_DIR, "templates")
+
+
 class Template:
-    pass
+    content: Content
+    path: str
+
+    def __init__(self, content: Content, name: Optional[str] = "default"):
+        self.content = content
+        self.path = os.path.join(name, "template.jinja")
+
+        full_path = os.path.join(TEMPLATES_DIR, self.path)
+        assert os.path.exists(full_path), f"Template not found: {name}"
+
+    def to_html(self) -> str:
+        env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
+        template = env.get_template(self.path)
+
+        content = self.content
+        return template.render(
+            title=content.title,
+            email=content.email,
+            phone=content.phone,
+        )
+
+    def to_pdf(self, output: str):
+        html = self.to_html()
+        output_file = os.path.join(output, "Resume.pdf")
+        HTML(string=html).write_pdf(output_file)
